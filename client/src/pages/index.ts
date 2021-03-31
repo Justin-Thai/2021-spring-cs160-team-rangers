60b8d4bfa4f5dfbebd963f59ec3e64cd487bc1bf
Irisssssss_ <Iris@Iriss-MacBook-Air-2.local>
2021-03-31 15:14:54 +0800

    fix SignUp file

--- client/src/pages/index.ts
+++ client/src/pages/index.ts
@@ -1,5 +1,5 @@
 import Home from './Home/Home';
-import SignUp from './SignUp';
+import SignUp from './SignUp/SignUp';
 import LogIn from './LogIn/LogIn';
 import Profile from './Profile/Profile';
 import PageNotFound from './PageNotFound/PageNotFound';
